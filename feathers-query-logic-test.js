61f7714047452710f71175b86ba94888697d97e7
Justin Meyer <justinbmeyer@gmail.com>
2018-12-17 18:49:12 -0600

    changing to tabs, ff 57

--- feathers-query-logic-test.js
+++ feathers-query-logic-test.js
@@ -3,84 +3,108 @@ var feathersQueryLogic = require('./feathers-query-logic');
 
 QUnit.module('feathers-query-logic');
 
-QUnit.test('toQuery', function(){
-    var query = feathersQueryLogic.toQuery({
-        $sort: {data: 1},
-        $populate: "foo",
-        $limit: 10,
-        $skip: 10,
-        complete: true
-    });
-
-    QUnit.deepEqual(query, {
-        sort: "data",
-        $populate: "foo",
-        page: {start: 10, end: 19},
-        filter: {complete: true}
+QUnit.test('toQuery', function() {
+	var query = feathersQueryLogic.toQuery({
+		$sort: {
+			data: 1
+		},
+		$populate: "foo",
+		$limit: 10,
+		$skip: 10,
+		complete: true
 	});
-	
+
+	QUnit.deepEqual(query, {
+		sort: "data",
+		$populate: "foo",
+		page: {
+			start: 10,
+			end: 19
+		},
+		filter: {
+			complete: true
+		}
+	});
+
 	// Test for $or
-    query = feathersQueryLogic.toQuery({
-        $sort: {data: 1},
-        $limit: 10,
+	query = feathersQueryLogic.toQuery({
+		$sort: {
+			data: 1
+		},
+		$limit: 10,
 		$skip: 10,
 		$or: [{
 			name: "matt"
 		}, {
 			name: "justin"
 		}]
-    });
+	});
 
-    QUnit.deepEqual(query, {
-        sort: "data",
-        page: {start: 10, end: 19},
-        filter: {
+	QUnit.deepEqual(query, {
+		sort: "data",
+		page: {
+			start: 10,
+			end: 19
+		},
+		filter: {
 			$or: [{
 				name: "matt"
 			}, {
 				name: "justin"
 			}]
 		}
-    });
+	});
 });
 
-QUnit.test('toParams', function(){
-    var query = feathersQueryLogic.toParams({
-        sort: "data",
-        $populate: "foo",
-        page: {start: 10, end: 19},
-        filter: {complete: true}
-    });
+QUnit.test('toParams', function() {
+	var query = feathersQueryLogic.toParams({
+		sort: "data",
+		$populate: "foo",
+		page: {
+			start: 10,
+			end: 19
+		},
+		filter: {
+			complete: true
+		}
+	});
 
-    QUnit.deepEqual(query, {
-        $sort: {data: 1},
-        $populate: "foo",
-        $limit: 10,
-        $skip: 10,
-        complete: true
-    });
+	QUnit.deepEqual(query, {
+		$sort: {
+			data: 1
+		},
+		$populate: "foo",
+		$limit: 10,
+		$skip: 10,
+		complete: true
+	});
 
 	// Test for $or
 	query = feathersQueryLogic.toParams({
-        sort: "data",
-        page: {start: 10, end: 19},
-        filter: {
+		sort: "data",
+		page: {
+			start: 10,
+			end: 19
+		},
+		filter: {
 			$or: [{
 				name: "matt"
 			}, {
 				name: "justin"
 			}]
 		}
-    });
+	});
 
-    QUnit.deepEqual(query, {
-        $sort: {data: 1},
-        $limit: 10,
-        $skip: 10,
-        $or: [{
+	QUnit.deepEqual(query, {
+		$sort: {
+			data: 1
+		},
+		$limit: 10,
+		$skip: 10,
+		$or: [{
 			name: "matt"
 		}, {
 			name: "justin"
 		}]
-    });
+	});
 });
